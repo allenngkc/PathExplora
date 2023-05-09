7dcf4ee746121f32be6c2e87e760cb983b5fcc20
allenng321 <pikachuexeallen@gmail.com>
2023-05-09 15:51:57 -0400

    Spritesheet Class Implementation

---
+++ src/base/spritesheet.py
@@ -0,0 +1,32 @@
+import pygame, os, json
+
+# Obtain file system directories
+current_dir = os.path.dirname(__file__)
+assets_dir = os.path.join(current_dir, "..\\..\\assets")
+
+class SpriteSheet:
+    def __init__(self, object, filename):
+        self.filename = filename
+        self.object = object
+        self.sprite_sheet = pygame.image.load(os.path.join(assets_dir, f"{object}\\{filename}.png")).convert()
+
+    # Returns the sprite to the screen given current cycle index (x, y, w, h) from parse_sprite()
+    def get_sprite(self, x, y, w, h):
+        sprite = pygame.Surface((w,h))
+        sprite.set_colorkey((0,0,0))
+        sprite.blit(self.sprite_sheet, (0, 0), (x, y, w, h))
+        return sprite
+
+    # Accessing json file to obtain positions of the spritesheet
+    def parse_sprite(self, index):
+        json_path = os.path.join(assets_dir, f"{self.object}\\{self.filename}.json")
+
+        with open(json_path, "r") as json_file:
+            data = json.load(json_file)
+
+        characters_keys = list(data["frames"].keys())
+        frame_obj = data["frames"][characters_keys[index]]["frame"]
+        print(frame_obj["x"])
+        return self.get_sprite(frame_obj["x"], frame_obj["y"], frame_obj["w"], frame_obj["h"])
+
+
--- src/player.py
+++ src/player.py
@@ -1,19 +1,32 @@
 import pygame
+from base.spritesheet import SpriteSheet
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
-        self.image = pygame.Surface((32,32))
-        self.image.fill('white')
+        self.image = pygame.Surface((16,48))
+        # self.image.fill('white')
         self.rect = self.image.get_rect(center = pos)
 
         self.direction = pygame.math.Vector2(0, 0)
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
 
+        self.display_surface = pygame.display.get_surface()
+
+        # Initialize character animations sprites
+        self.sprite_up = SpriteSheet("player", "player-up")
+        self.sprite_down = SpriteSheet("player", "player-down")
+        self.sprite_left = SpriteSheet("player", "player-left")
+        self.sprite_right = SpriteSheet("player", "player-right")
+
     def input(self):
         keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_p]:
+            print("Key P Pressed")
+            self.display_surface.blit(self.sprite_up.parse_sprite(1), (100, 50))
 
         if keys[pygame.K_w]:
             self.direction.y = -1
