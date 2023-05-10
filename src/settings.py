5090f331c97cc34b5d9d283f209e406741f6d0c0
allenng321 <pikachuexeallen@gmail.com>
2023-05-10 15:36:17 -0400

    Layer rendering metadata

--- src/settings.py
+++ src/settings.py
@@ -1,2 +1,16 @@
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 600
+
+LAYERS = {
+    'water': 0,
+    'ground': 1,
+    'soil': 2,
+    'soil water': 3,
+    'rain floor': 4,
+    'house bottom': 5,
+    'ground plant': 6,
+    'main': 7,
+    'house top': 8,
+    'fruit': 9,
+    'rain drops': 10
+}
