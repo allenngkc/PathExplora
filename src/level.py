2dc2ad1bcc3816ee486b7897a7a44f855ab753d5
allenng321 <pikachuexeallen@gmail.com>
2023-05-10 11:34:13 -0400

    Create Camera Group

--- src/level.py
+++ src/level.py
@@ -4,7 +4,7 @@ from player import Player
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = CameraGroup()
         self.setup()
 
     def setup(self):
@@ -12,5 +12,14 @@ class Level:
 
     def run(self, dt):
         self.display_surface.fill('black')
-        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.customize_draw()
         self.all_sprites.update(dt)
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+    def customize_draw(self):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image, sprite.rect)
