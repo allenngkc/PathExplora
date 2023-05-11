c9b58995f3fce98b18a0be38a01903ecc069c063
Allen <pikachuexeallen@gmail.com>
2023-05-11 00:28:00 -0400

    Tiled map change and added Cows

---
+++ assets/world/data/Tilesets/Cows.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Cows" tilewidth="89" tileheight="89" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="89" height="89" source="../../graphics/cow-0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="89" height="89" source="../../graphics/cow-1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="89" height="89" source="../../graphics/cow-2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="89" height="89" source="../../graphics/cow-3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="89" height="89" source="../../graphics/cow-4.png"/>
+ </tile>
+</tileset>
