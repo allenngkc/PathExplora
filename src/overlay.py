f930020eecd65f23e6d6177d2cadf150fe9d6f12
allenng321 <pikachuexeallen@gmail.com>
2023-05-23 15:29:52 -0400

    Update overlay

--- src/overlay.py
+++ src/overlay.py
@@ -6,13 +6,7 @@ current_dir = os.path.dirname(__file__)
 assets_dir = os.path.join(current_dir, "..\\assets")
 
 class Overlay:
-    def __init__(self, player):
+    def __init__(self):
         self.display_surface = pygame.display.get_surface()
-        self.player = player
-
-        self.ui_dialog = pygame.image.load(os.path.join(assets_dir, 'ui\\dialog.png')).convert_alpha()
-        
-    def display(self):
-        self.display_surface.blit(self.ui_dialog,(0,0))
 
         
