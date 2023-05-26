ccf30ee0116fa4d7b923db4c07887ff12c58813d
Allen <pikachuexeallen@gmail.com>
2023-05-25 23:46:18 -0400

    Added block replacement for start and end

--- src/base/grid_system.py
+++ src/base/grid_system.py
@@ -16,11 +16,21 @@ class GridSystem:
         # Selection of blocks for capturign user input
         self.blocks = [Block(143, 538), Block(190, 538)]
 
+        self.grass_img = pygame.image.load(os.path.join(assets_dir, 'ui\\grasstile.png')).convert_alpha()
+        self.wall_img = pygame.image.load(os.path.join(assets_dir, 'pathfinder\\Graphics\\bush.png')).convert_alpha()
+        self.start_img = pygame.image.load(os.path.join(assets_dir, 'ui\\soiltile.png')).convert_alpha()
+        self.end_img = pygame.image.load(os.path.join(assets_dir, 'ui\\stonetile.png')).convert_alpha()
+
         # Choosing the grid to start or end
         self.start_end = [Block(340, 538), Block(405, 538)]
 
         # Handles and modify path data 
-        self.cur_selected = 0 # 0 = grass, 1 = wall
+        self.cur_selected = 0 # 0 = grass, 1 = wall, 2 = start, 3 = end
+
+        # Saving previous block positions for start and end, could replace list tuple for better practice
+        self.prev_start = []
+        self.prev_end = []
+
 
         self.path_data = []
         for i in range(16):
@@ -75,9 +85,9 @@ class GridSystem:
 
     def on_click_startend(self, mouse_pos):
         if self.start_end[0].rect.collidepoint(mouse_pos):
-            print("Checked Start")
+            self.cur_selected = 2
         elif self.start_end[1].rect.collidepoint(mouse_pos):
-            print("Checked End")
+            self.cur_selected = 3
         
 
     # On click cell, calculates the specific grid cell by obtaining the mouse position
@@ -85,19 +95,54 @@ class GridSystem:
         for row in self.grids:
             for cell in row:
                 if cell.rect.collidepoint(mouse_pos):
-                    cell_row = int((cell.x-GRID_INIT_X)/30)
-                    cell_col = int((cell.y-GRID_INIT_Y)/30)
+                    cell_col = int((cell.x-GRID_INIT_X)/30)
+                    cell_row = int((cell.y-GRID_INIT_Y)/30)
 
                     cur = None
                     if self.cur_selected == 0:
-                        cur = pygame.image.load(os.path.join(assets_dir, 'ui\\grasstile.png')).convert_alpha()
-                    else:
-                        cur = pygame.image.load(os.path.join(assets_dir, 'pathfinder\\Graphics\\bush.png')).convert_alpha()
-                    self.grids[cell_row][cell_col].update_image(cur)
+                        cur = self.grass_img
+
+                        self.grids[cell_row][cell_col].update_image(cur)
+                    elif self.cur_selected == 1:
+                        cur = self.wall_img
+                        self.grids[cell_row][cell_col].update_image(cur)
+
+                    elif self.cur_selected == 2 and self.path_data[cell_row][cell_col] == 0 and self.path_data[cell_row][cell_col] != 2: # Start can only be placed on grass
+                        cur = self.start_img
+
+                        # Since only 1 starting position exists, if a start block is already been placed, replace it previous one with grass
+                        if self.prev_start == []:
+                            self.prev_start = [cell_row, cell_col]
+                        else:
+                            self.grids[self.prev_start[0]][self.prev_start[1]].update_image(self.grass_img)
+                            self.path_data[self.prev_start[0]][self.prev_start[1]] = 0
+                            self.prev_start = [cell_row, cell_col]
+
+                        self.grids[cell_row][cell_col].update_image(cur)
+
+                    elif self.cur_selected == 3 and self.path_data[cell_row][cell_col] == 0 and self.path_data[cell_row][cell_col] != 3: # End can only be placed on grass
+                        cur = self.end_img
+
+                        if self.prev_end == []:
+                            self.prev_end = [cell_row, cell_col]
+                        else:
+                            self.grids[self.prev_end[0]][self.prev_end[1]].update_image(self.grass_img)
+                            self.path_data[self.prev_end[0]][self.prev_end[1]] = 0
+                            self.prev_end = [cell_row, cell_col]
+
+                        self.grids[cell_row][cell_col].update_image(cur)
 
                     # Modify path_data for backend
+                    print(f"Row: {cell_row}, Col: {cell_col}")
+                    print(f"Val: {self.path_data[cell_row][cell_col]}")
                     self.path_data[cell_row][cell_col] = self.cur_selected
 
+                    for row in self.path_data:
+                        for cell in row:
+                            print(cell, end=" ")
+                        print()
+                    print("-----------------------------", end="\n")
+                
 class GridCell:
     def __init__(self, x, y, image=None):
         self.x = x
